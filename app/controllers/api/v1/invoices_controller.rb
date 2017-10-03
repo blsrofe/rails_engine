812ba45c1783fbd5f55159266dc160f3b55b10a1
erose357 <erose357@gmail.com>
2017-10-03 08:30:46 -0600

    Add test for invoices show page

--- app/controllers/api/v1/invoices_controller.rb
+++ app/controllers/api/v1/invoices_controller.rb
@@ -6,6 +6,10 @@ module Api
         render json: Invoice.all
       end
 
+      def show
+        render json: Invoice.find(params[:id])
+      end
+
     end
   end
 end
--- spec/requests/api/v1/invoices_request_spec.rb
+++ spec/requests/api/v1/invoices_request_spec.rb
@@ -1,14 +1,28 @@
 require 'rails_helper'
 
 describe "Invoices API" do
-  it "sends a list of items" do
-    create_list(:invoice, 3)
+  context "GET /invoices" do
+    it "sends a list of invoices" do
+      create_list(:invoice, 3)
 
-    get '/api/v1/invoices.json'
+      get '/api/v1/invoices.json'
 
-    invoices = JSON.parse(response.body)
+      invoices = JSON.parse(response.body)
 
-    expect(response).to be_success
-    expect(invoices.count).to eq(3)
+      expect(response).to be_success
+      expect(invoices.count).to eq(3)
+    end
+
+    it "sends one invoice" do
+      invoice = create(:invoice)
+
+      get "/api/v1/invoices/#{invoice.id}"
+
+      inv = JSON.parse(response.body)
+      
+      expect(response).to be_success
+      expect(inv["status"]).to eq(invoice.status)
+      expect(inv["merchant_id"]).to eq(invoice.merchant_id)
+    end
   end
 end
