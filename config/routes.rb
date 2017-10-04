86d89975de55a6730944537b300dd3ea0d50c11e
Becki Srofe <blf_23@yahoo.com>
2017-10-03 19:55:01 -0600

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,15 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      namespace :merchants do
-        get "random", to: 'random#show', as: 'random'
-      end
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
-      namespace :merchants do
-        get "find", to: 'search#show', as: 'find'
-        get "find_all", to: 'search#index', as: 'find_all'
-      end
-    end
-  end
-
-  namespace :api do
-    namespace :v1 do
       namespace :invoices do
         get '/find', to: 'search#show'
         get '/find_all', to: 'search#index'
         get '/random', to: 'random#show'
+      end
+      namespace :merchants do
+        get "random", to: 'random#show', as: 'random'
+        get "find", to: 'search#show', as: 'find'
+        get "find_all", to: 'search#index', as: 'find_all'
       end
       resources :merchants, only: [:index, :show]
       resources :invoices, only: [:index, :show]
