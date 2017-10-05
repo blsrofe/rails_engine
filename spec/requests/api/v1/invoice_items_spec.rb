c5adecc10bae5d87c8d00b7323d81c996b05ecbd
erose357 <erose357@gmail.com>
2017-10-04 22:06:44 -0600

    Adds random controller action for invoice items

--- spec/requests/api/v1/invoice_items_spec.rb
+++ spec/requests/api/v1/invoice_items_spec.rb
@@ -36,5 +36,28 @@ RSpec.describe "Invoice Items API" do
       expect(invoice_item.quantity).to eq(json["quantity"])
       expect(invoice_item.invoice_id).to eq(json["invoice_id"])
     end
+
+    it "can find multiple items" do
+      invoice_items = create_list(:invoice_item, 3)
+
+      get "/api/v1/invoice_items/find_all?quantity=#{invoice_items[1].quantity}"
+
+      json = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(json.count).to eq(3)
+      expect(json.class).to be(Array)
+    end
+
+    it "can find a random item" do
+      invoice_items = create_list(:invoice_item, 5)
+
+      get "/api/v1/invoice_items/random.json"
+
+      json = JSON.parse(response.body)
+
+      expect(response).to be_success
+      expect(json.class).to be(Hash)
+    end
   end
 end
