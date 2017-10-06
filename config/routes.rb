404a9910404a4281176520a33c9841c63c15c8d2
erose357 <erose357@gmail.com>
2017-10-06 09:24:57 -0600

    Fixes routing bug

--- config/routes.rb
+++ config/routes.rb
@@ -45,7 +45,7 @@ Rails.application.routes.draw do
         get '/:id/invoice_items', to: 'invoice_items#index'
         get '/:id/merchant', to: 'merchants#show'
         get '/most_revenue', to: 'most_revenue#index'
-        get '/:most_items', to: 'most_items#index'
+        get '/most_items', to: 'most_items#index'
         get '/:id/best_day', to: 'best_day#show'
       end
       namespace :invoice_items do
