00b5ae680c9ddaf9b661dabdf7a4a62c86e748d2
erose357 <erose357@gmail.com>
2017-10-05 18:45:46 -0600

    Adds route and controller for merchants bi most_revenue

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
         get '/:id/items', to: 'items#index'
         get '/:id/invoices', to: 'invoices#index'
         get '/:id/favorite_customer', to: 'favorite_customer#show'
+        get '/most_revenue', to: 'most_revenue#index'
       end
       namespace :transactions do
         get '/find', to: 'search#show'
